6b5a8076ee94cbd2db761f2c1dce4372b10bafe1
SonTD <sontddeveloper@email.com>
2023-03-27 23:37:02 +0700

    departmentId change into name

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { format } from 'date-fns'
+import { format } from "date-fns";
+
 const urlStaffs =
   "http://localhost:8080/staffs?fbclid=IwAR3CBsYwQWXxZO4t9pNyN6SOWvOTR_4yfuTSVkpDZV_qoECYvZn_w-rulFo";
+const urlDepartments = "http://localhost:8080/departments";
 
-  const urlDepartments ="http://localhost:8080/departments"
 function App() {
   const [loading, setLoading] = useState(true);
   const [staffs, setStaffs] = useState([]);
   const [value, setValue] = useState(0);
+  const [departments, setDepartments] = useState([]);
 
   const fetchStaffs = async () => {
-    const reponse = await fetch(urlStaffs);
-    const newStaffs = await reponse.json();
+    const response = await fetch(urlStaffs);
+    const newStaffs = await response.json();
     setStaffs(newStaffs);
-
-
-    setStaffs(newStaffs);
-
     setLoading(false);
   };
+
+  const fetchDepartments = async () => {
+    const response = await fetch(urlDepartments);
+    const newDepartments = await response.json();
+    setDepartments(newDepartments);
+  };
+
   useEffect(() => {
     fetchStaffs();
+    fetchDepartments();
   }, []);
 
   if (loading) {
@@ -33,36 +39,35 @@ function App() {
 
   const { name, doB, startDate, departmentId, annualLeave, overTime } =
     staffs[value];
+  const departmentName = departments.find(
+    (dept) => dept.id === departmentId
+  )?.name;
   return (
     <section className="section">
       <div className="title">
-        <h2> Ứng dụng quản lý nhân sự v.0.1 </h2>
+        <h2>Ứng dụng quản lý nhân sự v.0.1</h2>
         <div className="underline"></div>
       </div>
-      <div className="staffs-name">
-        {/* btn container */}
-        <div className="btn-container">
-          {staffs.map((item, index) => {
-            return (
-              <button
-                key={item.id}
-                onClick={() => setValue(index)}
-                className={`staffs-btn ${index === value && "active-btn"}`}
-              >
-                {item.name}
-              </button>
-            );
-          })}
-          
-          <article className="staffs-info">
-            <h3> Họ Và Tên: {name}</h3>
-            <p>Ngày sinh: {format(new Date(doB), 'do MMMM Y  H:m:s')}</p>
-            <p>Ngày vào công ty: {format(new Date(startDate), 'do MMMM Y  H:m:s')}</p>
-            <p>Phòng ban: {departmentId}</p>
-            <p>Số ngày nghỉ còn lại: {annualLeave}</p>
-            <p>Số ngày làm thêm: {overTime}</p>
-          </article>
+      <div className="staffs-container">
+        <div className="staffs-btn-container">
+          {staffs.map((staff, index) => (
+            <button
+              key={staff.id}
+              className={`staffs-btn ${index === value && "active-btn"}`}
+              onClick={() => setValue(index)}
+            >
+              {staff.name}
+            </button>
+          ))}
         </div>
+        <article className="staffs-info">
+          <h3>Họ và Tên: {name}</h3>
+          <p>Ngày sinh: {format(new Date(doB), "do MMMM Y  H:m:s")}</p>
+          <p>Ngày vào công ty: {format(new Date(startDate), "do MMMM Y  H:m:s")}</p>
+          <p>Phòng ban: {departmentName}</p>
+          <p>Số ngày nghỉ còn lại: {annualLeave}</p>
+          <p>Số ngày làm thêm: {overTime}</p>
+        </article>
       </div>
     </section>
   );
